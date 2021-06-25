1d194d595cb739c7b4df88ef73c8a083e80344d0
SanketTambe39 <sankettambe39@gmail.com>
2021-06-25 20:16:07 +0530

    ADDED: Checked space is available or not.

--- TicTacToeSimulator/src/com/bridgelabz/tictactoesimulator/TicTacToeSimulator.java
+++ TicTacToeSimulator/src/com/bridgelabz/tictactoesimulator/TicTacToeSimulator.java
@@ -1,45 +1,47 @@
 package com.bridgelabz.tictactoesimulator;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TicTacToeSimulator {
 	
 	static Scanner scanner = new Scanner(System.in);
-	static char[] board = new char[9];
+	static String[] board = new String[9];
 	
 	public static void main(String[] args) {
 		System.out.println("========= Welcome to Tic Tac Toe Simulator Game =======");
 		
-		char playerchoice = chooseLetter();
-		char computerchoice = ( playerchoice == 'X' ) ? 'O' : 'X';
+		String playerChoice = chooseLetter();
+		//String computerChoice = "X";
+		String computerChoice = ( playerChoice.equalsIgnoreCase("X")) ? "O" : "X";
 		
-		System.out.println("Player choice is ::"+playerchoice);
-		System.out.println("Computer choice is ::"+computerchoice);
 		
-		char[] board=createBoard();
+		System.out.println("Player choice is ::"+playerChoice);
+		System.out.println("Computer choice is ::"+computerChoice);
+		
+		String[] board=createBoard();
 		
 		System.out.println("Your Blank Gamming Board is here");
 		showBoard();
 		
 		while(checkspace(board))
 		{
-			playerPlace(board,playerchoice);
+			playerPlace(board,playerChoice);
 		}
-		
 	}
 	
 	/* UC1 */
-	private static char[] createBoard() {
+	private static String[] createBoard() {
 		for(int i=0;i<board.length;i++) {
-			board[i] = ' ';
+			board[i] = " ";
 		}
 		return board;
 	}
 	/* UC2 */
-	private static char chooseLetter() {
+	private static String chooseLetter() {
 		
 		System.out.println("Enter your choice X or O ::");
-		char playerturn=scanner.next().toUpperCase().charAt(0);
+		String playerturn=scanner.next().toUpperCase();
 		return playerturn;
 	
 	}
@@ -54,7 +56,7 @@ public class TicTacToeSimulator {
 		System.out.println("\t/---|---|---\\");
 	}
 	/* UC4 */
-	private static void playerPlace(char board[],char playerchoice)
+	private static void playerPlace(String[] board2,String playerchoice)
 	{
 		
 		boolean playerplay = true;
@@ -64,14 +66,14 @@ public class TicTacToeSimulator {
 			System.out.println("Enter where player want to place his symbol :: ");
 			int playerplace = scanner.nextInt()-1;
 			
-			if(board[playerplace] == ' ')
+			if(board2[playerplace] == " ")
 			{
-				board[playerplace] = playerchoice;
+				board2[playerplace] = playerchoice;
 				playerplay = false;
 			}
 			else
 			{
-				System.out.println("Opps !! You just Put the wrong Input");
+				System.out.println("Opps !! You just tried to Cheat");
 			}
 			
 		}
@@ -80,23 +82,25 @@ public class TicTacToeSimulator {
 	}
 	
 	/* UC5 */
-	private static boolean checkspace(char[] board) {
+	private static boolean checkspace(String[] board2) {
 		
 		boolean isSpaceOccupied = true;
 		int numberOfFreeSpace = 0;
 		
-		for (int index = 1; index < board.length; index++)
+		for (int index = 1; index < board2.length; index++)
 		{
-			if(board[index]== ' ')
+			if(board2[index] == " ")
 			{
 				numberOfFreeSpace++;
 			}
 		}
 		if(numberOfFreeSpace == 0)
 		{
+			System.out.println("Board is full.");
 			isSpaceOccupied=false;
 		}
 		return isSpaceOccupied;
 	}
 
+	
 }
