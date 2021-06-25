b2cd2b57d6302e3fab7ffb53b60068bbd8b11eb9
SanketTambe39 <sankettambe39@gmail.com>
2021-06-25 10:42:45 +0530

    ADDED: Choice Of Word Given To User

--- TicTacToeSimulator/src/com/bridgelabz/tictactoesimulator/TicTacToeSimulator.java
+++ TicTacToeSimulator/src/com/bridgelabz/tictactoesimulator/TicTacToeSimulator.java
@@ -1,10 +1,24 @@
 package com.bridgelabz.tictactoesimulator;
 
-public class TicTacToeSimulator {
+import java.util.Scanner;
 
+public class TicTacToeSimulator {
+	
 	public static void main(String[] args) {
-		System.out.println("=========Welcome to Tic Tac Toe Simulator Game =======");
+		System.out.println("========= Welcome to Tic Tac Toe Simulator Game =======");
 		char[] board=createBoard();
+		
+		char playerchoice = chooseLetter();
+		
+		System.out.println("you choose ::"+playerchoice);
+		if(playerchoice == 'X') {
+			char computerchoice = 'O';
+			System.out.println("Computer Will be :: "+computerchoice);
+		}
+		else {
+			char computerchoice = 'Y';
+			System.out.println("Computer Will be :: "+computerchoice);
+		}
 	}
 	
 	/* UC1 */
@@ -15,4 +29,17 @@ public class TicTacToeSimulator {
 		}
 		return board;
 	}
+	/* UC2 */
+	private static char chooseLetter() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your choice X or O ::");
+		
+		char playerturn=scanner.next().toUpperCase().charAt(0);
+		
+		return playerturn;
+	
+	}
+	
+	
+
 }
