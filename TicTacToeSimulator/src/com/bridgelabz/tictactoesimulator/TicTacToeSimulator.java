3158a924ba3168ac2ae7d4bed6f748f1c0610263
SanketTambe39 <sankettambe39@gmail.com>
2021-06-25 09:32:36 +0530

    ADDED: Board to the game

--- TicTacToeSimulator/src/com/bridgelabz/tictactoesimulator/TicTacToeSimulator.java
+++ TicTacToeSimulator/src/com/bridgelabz/tictactoesimulator/TicTacToeSimulator.java
@@ -4,6 +4,15 @@ public class TicTacToeSimulator {
 
 	public static void main(String[] args) {
 		System.out.println("=========Welcome to Tic Tac Toe Simulator Game =======");
+		char[] board=createBoard();
 	}
 	
+	/* UC1 */
+	private static char[] createBoard() {
+		char[] board = new char[9];
+		for(int i=0;i<board.length;i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
 }
